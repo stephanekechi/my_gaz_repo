a023fbffbf1a90c791e61749017c23a4cd1b0ab9
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-05-10 21:12:21 +0200

    fix build dataaccess module

---
+++ gaz-repo-dataaccess/src/main/java/com/masteranythink/gazrepodataaccess/GazRepoDataaccessApplication.java
@@ -0,0 +1,13 @@
+package com.masteranythink.gazrepodataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazRepoDataaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazRepoDataaccessApplication.class, args);
+	}
+
+}
